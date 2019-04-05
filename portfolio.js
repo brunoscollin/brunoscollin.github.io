2ce41f4f8f8609c7efafc5c74fd784bc81d9183d
brunoscollin <brunoscollin@gmail.com>
2019-04-05 13:33:26 +0200

    clique image site

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,27 @@
 function myFunction() {
     location.href = "http://www.coding-academy.fr/web-go/";
   }
+
+  function showHotel() {
+    location.href = "hotel.html";
+  }
+
+  function showForest() {
+    location.href = "accessibility.html";
+  }
+
+  function showWorld() {
+    location.href = "helloworld.html";
+  }
+
+  function showMoon() {
+    location.href = "moon.html";
+  }
+
+  function showSafy() {
+    location.href = "safyslide.html";
+  }
+
+  function showBat() {
+    location.href = "licorne.html";
+  }
